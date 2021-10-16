0cdcdcb2c2edead065d663e47612b1152399d529
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-10-16 21:31:19 +0100

    work on the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-echo "deploy.sh: hello"
-
 REPOSITORY=releases
 REPOSITORYID=releases
 
 GROUPID=com.rsmaxwell.players
-ARTIFACTID=players-tt-api-amd64-linux
+ARTIFACTID=players-tt-api_amd64-linux
 VERSION=${BUILD_ID}
 PACKAGING=zip
 
 URL=https://server.rsmaxwell.co.uk/archiva/repository/${REPOSITORY}
 
-FILENAME=${ARTIFACTID}-${VERSION}.${PACKAGING}
+FILENAME=${ARTIFACTID}_${VERSION}.${PACKAGING}
 
 rm -rf ~/workspace/players-tt-api_main/deploy
 mkdir -p ~/workspace/players-tt-api_main/deploy
 
 cd ~/workspace/players-tt-api_main/bin
-zip ../deploy/${FILENAME} ${ARTIFACTID}
+zip ../deploy/${FILENAME} players-tt-api
 
-# mvn deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -Dpackaging=${PACKAGING} -Dfile=${FILENAME} -DrepositoryId=${REPOSITORYID} -Durl=${URL} -DrepositoryId=${REPOSITORYID}
+cd ~/workspace/players-tt-api_main/deploy
+mvn deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -Dpackaging=${PACKAGING} -Dfile=${FILENAME} -DrepositoryId=${REPOSITORYID} -Durl=${URL} -DrepositoryId=${REPOSITORYID}
 
