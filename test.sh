152ac9a9295c64d8e68b0dc22098841c2bfea42d
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-10-16 16:06:42 +0100

    setup for testing

--- test.sh
+++ test.sh
@@ -15,9 +15,9 @@ export AccessTokenExpiry="5h"
 export RefreshTokenExpiry="10h"
 export ClientRefreshTimer="30s"
 
-echo "Config file: $1"
+echo "Config file:"
 cat $1
 
 cd ~/workspace/players-tt-api_main/bin
 
-players-tt-api --config $1
+./players-tt-api --config $1
