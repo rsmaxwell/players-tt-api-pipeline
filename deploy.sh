922b873385e9b3b55bb7c75bce4fde9b1e7b0083
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-10-16 21:13:59 +0100

    work on the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo "deploy.sh: hello"
+
+REPOSITORY=releases
+REPOSITORYID=releases
+
+GROUPID=com.rsmaxwell.players
+ARTIFACTID=players-tt-api-amd64-linux
+VERSION=${BUILD_ID}
+PACKAGING=zip
+
+URL=https://server.rsmaxwell.co.uk/archiva/repository/${REPOSITORY}
+
+FILENAME=${ARTIFACTID}-${VERSION}.${PACKAGING}
+
+rm -rf ~/workspace/players-tt-api_main/deploy
+mkdir -p ~/workspace/players-tt-api_main/deploy
+
+cd ~/workspace/players-tt-api_main/bin
+zip ../deploy/${FILENAME} ${ARTIFACTID}
+
+# mvn deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -Dpackaging=${PACKAGING} -Dfile=${FILENAME} -DrepositoryId=${REPOSITORYID} -Durl=${URL} -DrepositoryId=${REPOSITORYID}
+
