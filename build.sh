9d16e0f4fca0c6155285b86e07f28f04f9d2ac1a
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-10-16 14:20:11 +0100

    set gopath

--- build.sh
+++ build.sh
@@ -8,15 +8,20 @@ echo "    GOROOT=$(go env GOROOT)"
 echo "    GOPATH=$(go env GOPATH)"
 
 go get github.com/dgrijalva/jwt-go
-go get github.com/eclipse/paho.mqtt.golang
-go get github.com/go-playground/locales/en
-go get github.com/go-playground/universal-translator
-go get github.com/jackc/pgconn
-go get github.com/jackc/pgx
-go get github.com/jackc/pgx/stdlib
-go get github.com/lib/pq
-go get golang.org/x/crypto/bcrypt
-go get gopkg.in/go-playground/validator.v9
-go get gopkg.in/go-playground/validator.v9/translations/en
 
-go install ./...
+
+
+
+
+# go get github.com/eclipse/paho.mqtt.golang
+# go get github.com/go-playground/locales/en
+# go get github.com/go-playground/universal-translator
+# go get github.com/jackc/pgconn
+# go get github.com/jackc/pgx
+# go get github.com/jackc/pgx/stdlib
+# go get github.com/lib/pq
+# go get golang.org/x/crypto/bcrypt
+# go get gopkg.in/go-playground/validator.v9
+# go get gopkg.in/go-playground/validator.v9/translations/en
+# 
+# go install ./...
